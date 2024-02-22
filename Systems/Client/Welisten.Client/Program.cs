1f19ec9d3a77f800dd41985cbf134a771a4b0a37
sardorshukurov <sardor.2002sh@gmail.com>
2024-02-22 22:32:12 +0500

    Update[Home.razor]. Added a card

--- Systems/Client/Welisten.Client/Program.cs
+++ Systems/Client/Welisten.Client/Program.cs
@@ -8,9 +8,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddMudServices();
 
 
 await builder.Build().RunAsync();
