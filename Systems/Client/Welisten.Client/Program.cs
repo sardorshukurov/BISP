929b74303e4186ff1af7f222c3f879c6aca3833e
sardorshukurov <102730798+sardorshukurov@users.noreply.github.com>
2024-02-24 15:49:59 +0500

    Fix[Client]. DI

--- Systems/Client/Welisten.Client/Program.cs
+++ Systems/Client/Welisten.Client/Program.cs
@@ -2,15 +2,16 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Welisten.Client;
 using MudBlazor.Services;
+using Welisten.Client.Pages.Posts.Services;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(Settings.ApiRoot) });
 
 builder.Services.AddMudServices();
-
+builder.Services.AddScoped<IPostService, PostService>();
 
 await builder.Build().RunAsync();
