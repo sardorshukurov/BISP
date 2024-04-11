650c12fa707af7546ef6f7f647063a0d002dcc72
sardorshukurov <sardor.2002sh@gmail.com>
2024-04-12 01:54:48 +0500

    Update[register,login]. Added validation of the form

--- Systems/Client/Welisten.Client/Pages/Authorization/Models/LoginModel.cs
+++ Systems/Client/Welisten.Client/Pages/Authorization/Models/LoginModel.cs
@@ -20,4 +20,12 @@ public class LoginValidator : AbstractValidator<LoginModel>
             .MinimumLength(8).WithMessage("Password should have at least 8 characters.")
             .MaximumLength(50).WithMessage("Password should have at most 50 characters.");
     }
+    
+    public Func<object, string, Task<IEnumerable<string>>> ValidateValue => async (model, propertyName) =>
+    {
+        var result = await ValidateAsync(ValidationContext<LoginModel>.CreateWithOptions((LoginModel)model, x => x.IncludeProperties(propertyName)));
+        if (result.IsValid)
+            return Array.Empty<string>();
+        return result.Errors.Select(e => e.ErrorMessage);
+    };
 }
